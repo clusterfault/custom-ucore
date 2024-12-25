41fe78e757780daaeff7f08dc3a7e5686ebcbd3b
Harpreet Singh <harpreet.surdy@gmail.com>
2024-12-24 19:46:57 -0800

    Make build script dynamically run install and post-install scripts for packages

--- build.sh
+++ build.sh
@@ -13,17 +13,18 @@ RELEASE="$(rpm -E %fedora)"
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-rpm-ostree install screen
+# rpm-ostree install screen
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
 
-# install homebrew
-/tmp/packages/brew/install.sh
+packages=$(find /tmp/packages -maxdepth 1 ! -path /tmp/packages  -type d -exec basename {} \;)
 
-#### Example for enabling a System Unit File
+# install packages
+for package in $packages; do
+    # Script to install the package
+    /tmp/packages/${package}/install.sh
 
-systemctl enable podman.socket
-
-# homebrew post-install tasks
-/tmp/packages/brew/post-install.sh
+    # Script to run post-install tasks
+    /tmp/packages/${package}/post-install.sh
+done
